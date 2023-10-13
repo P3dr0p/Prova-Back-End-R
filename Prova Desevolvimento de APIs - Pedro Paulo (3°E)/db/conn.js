51d0c0159bd9bf646f1d7be2bfd8ff69691efd7f
Pedro Paulo Ramalho de Almeida <108875939+P3dr0p@users.noreply.github.com>
2023-10-13 12:30:45 -0300

    Update conn.js

--- Prova Desevolvimento de APIs - Pedro Paulo (3°E)/db/conn.js
+++ Prova Desevolvimento de APIs - Pedro Paulo (3°E)/db/conn.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2')
 //Configuração e conexão com banco
 const conn = mysql.createPool({
   connectionLimit: 10,
-  host: '',
+  host: 'localhost',
   port: '', // É opcional.
   user: 'aluno_medio',
   password: '@lunoSenai23.',
-  database: '',
+  database: 'livraria',
 })
 
 // É necessário exporta esse modulo
